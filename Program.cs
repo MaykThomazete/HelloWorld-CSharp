b8ee2b522c889b191a200f33495c9b6fe57544df
maykhomazete <maykthomazete@alunosenai.brgit config --global user.email>
2025-07-31 10:32:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(" A soma de " + numero1 + " e " + numero2 + " é: " + soma);
+    }
+}
